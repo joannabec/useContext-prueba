f8abe59800e063b2edc035e24d2d059ff0e14581
mariebec <mariela.benitezc9@gmail.com>
2020-10-05 11:04:19 -0500

    build

---
+++ docs/precache-manifest.351694ac6afbaf568d1997a4881d9f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1e5d5262dc3657a727f40fffd91e6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f7270e81a53eabcf7a4",
+    "url": "/static/css/main.cb690266.chunk.css"
+  },
+  {
+    "revision": "6d2727061a81d6691fd7",
+    "url": "/static/js/2.dce56939.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dce56939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7270e81a53eabcf7a4",
+    "url": "/static/js/main.6d070c97.chunk.js"
+  },
+  {
+    "revision": "f28a22e86cfa7bbbc9e8",
+    "url": "/static/js/runtime-main.18f349b0.js"
+  }
+]);
