be2b195593bf042d0736313aa10ebdcfa34715a6
mariebec <mariela.benitezc9@gmail.com>
2020-10-05 10:57:58 -0500

    Agregando directorio docs

---
+++ docs/precache-manifest.3c24d336178753a1c102f72c74d8553b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771beca875efb274138e34d26814c45a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f27ff274e796fd97e02f",
+    "url": "/static/css/main.cb690266.chunk.css"
+  },
+  {
+    "revision": "f624284c86344b2fb9f4",
+    "url": "/static/js/2.896f2f6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.896f2f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f27ff274e796fd97e02f",
+    "url": "/static/js/main.fd2eed19.chunk.js"
+  },
+  {
+    "revision": "f28a22e86cfa7bbbc9e8",
+    "url": "/static/js/runtime-main.18f349b0.js"
+  }
+]);
